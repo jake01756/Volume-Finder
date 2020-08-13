7bfc0ecc7ae53adec3eb52f6f804ab8ee1f1d428
jake01756 <jake01756@penguin>
2020-08-13 15:38:01 -0400

    Added files

---
+++ volume_finder/VolumeFinderWindow.py
@@ -0,0 +1,28 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+### BEGIN LICENSE
+# This file is in the public domain
+### END LICENSE
+
+from locale import gettext as _
+
+from gi.repository import Gtk # pylint: disable=E0611
+import logging
+logger = logging.getLogger('volume_finder')
+
+from volume_finder_lib import Window
+from volume_finder.AboutVolumeFinderDialog import AboutVolumeFinderDialog
+from volume_finder.PreferencesVolumeFinderDialog import PreferencesVolumeFinderDialog
+
+# See volume_finder_lib.Window.py for more details about how this class works
+class VolumeFinderWindow(Window):
+    __gtype_name__ = "VolumeFinderWindow"
+    
+    def finish_initializing(self, builder): # pylint: disable=E1002
+        """Set up the main window"""
+        super(VolumeFinderWindow, self).finish_initializing(builder)
+
+        self.AboutDialog = AboutVolumeFinderDialog
+        self.PreferencesDialog = PreferencesVolumeFinderDialog
+
+        # Code for other initialization actions should be added here.
+
