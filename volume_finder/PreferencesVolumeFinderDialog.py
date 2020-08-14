0cc18543bd17770925d5c5742541749407476261
jake01756 <jake01756@penguin>
2020-08-14 18:29:24 -0400

    Added a license

--- volume_finder/PreferencesVolumeFinderDialog.py
+++ volume_finder/PreferencesVolumeFinderDialog.py
@@ -1,6 +1,17 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 ### BEGIN LICENSE
-# This file is in the public domain
+# Copyright (C) 2020 Jake Thomas jake01756@gmail.com
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 ### END LICENSE
 
 # This is your preferences dialog.
